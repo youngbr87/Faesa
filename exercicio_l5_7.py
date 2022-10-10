2137388c9c18244fd7ad318ed78b85d75c94f071
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-10-10 16:12:07 -0300

    exercicio_7

---
+++ exercicio_l5_7.py
@@ -0,0 +1,15 @@
+#exericio unidade 3
+#Professora Renata Laranja
+#Desvio de condicional composto e aninhado
+#if elif else
+#Lista 5
+#Exercicio 7
+
+while True:
+    
+    
+    
+    
+    resp = str(input('Deseja continuar ? [S]/[N] =>'))
+    if resp in 'N,n':
+        break
