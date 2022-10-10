0f632a2118482ae40c9b8433f3c1289246e66b36
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-10-10 14:48:48 -0300

    Adcionado projetos

---
+++ python_l5_6.py
@@ -0,0 +1,16 @@
+#exericio unidade 3
+#Professora Renata Laranja
+#Desvio de condicional composto e aninhado
+#if elif else
+#Lista 5
+#Exercicio 6
+
+while True:
+    
+    str(print('---------------------------'))
+    str(print('  '))
+    str(print('---------------------------'))
+    
+    resp = str(input('Deseja continuar ? [S]/[N] => '))
+    if resp in 'N,n':
+        break
