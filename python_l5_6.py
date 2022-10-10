1f225001a07d2743afefe0a1e6668ff391eaa5ce
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-10-10 15:21:21 -0300

    codigo pronto

--- python_l5_6.py
+++ python_l5_6.py
@@ -8,9 +8,22 @@
 while True:
     
     str(print('---------------------------'))
-    str(print(' teste '))
+    str(print('       Nota de aluno       '))
     str(print('---------------------------'))
+    print()
+    nome = str(input('Digite o nome do aluno => '))
+    n_trab = float(input('Digite a nota do trablho => '))
+    n_prov = float(input('Digite a nota da prova => '))
     
+    media = (n_prov + n_trab) / 2
+    
+    print()
+    
+    if media > 7:
+        str(print('O aluno', nome, 'está aprovado'))
+    else:    
+        str(print('O aluno', nome, 'está reprovado'))
+    print()
     resp = str(input('Deseja continuar ? [S]/[N] => '))
     if resp in 'N,n':
         break
