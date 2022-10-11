e3813013cea16dcc3e6d2e58b22f273364761dda
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-10-11 16:11:32 -0300

    Update exercicio_l5_7.py

--- exercicio_l5_7.py
+++ exercicio_l5_7.py
@@ -7,8 +7,16 @@
 
 while True:
     
+    str(print('-----------------------------'))
+    str(print(' Diferença entre 2 numeros   '))
+    str(print('-----------------------------'))
     
+    num1 = float(input('Digite um número => '))
+    num2 = float(input('Digite outro número => '))
     
+    dif = (num1 - num2)
+    
+    str(print('A diferença entre os dois números é ', dif))
     
     resp = str(input('Deseja continuar ? [S]/[N] =>'))
     if resp in 'N,n':
