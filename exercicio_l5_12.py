05404e7c28784a9891af46bd27564bc900e5b7ae
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-10-17 11:30:39 -0300

    versao 1.0

--- exercicio_l5_12.py
+++ exercicio_l5_12.py
@@ -17,14 +17,27 @@ while True:
     print()
     lado3 = int(input('Digite o valor do lado 3 => '))
     
+    print()
+    
     if lado1 == 0 or lado2 == 0 or lado3 == 0:
         str(print('Não é um triangulo'))
+    
+    elif lado1 > (lado2 + lado3) or lado2 > (lado1 + lado2) or lado3 > (lado1 + lado2):
+        str(print('Não é um triangulo'))
         
+    elif lado1 == lado2 and lado2 == lado3 and lado1 == lado3:
+        str(print('É um triangulo equilatreo'))
+       
+    elif lado1 != lado2 and lado2 != lado3 and lado1 != lado3:
+        str(print('É um triangulo escaleno')) 
+    
+    else:
+        str(print('É um triangulo isoceles'))
         
     
-    
-    
+    print()    
+        
     
     resp = str(input('Deseja continuar ? [S]/[N] => '))
-       if resp in 'N,n':
-           break 
+    if resp in 'N,n':
+        break 
