2238f5ca90247aeafd6bc236ce6702dd0b94851b
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-10-19 19:42:36 -0300

    versao 1.0

---
+++ exercicio_l6_2.py
@@ -0,0 +1,19 @@
+#exericio unidade 3
+#Professora Renata Laranja
+#Desvio de condicional composto e aninhado
+#while for
+#Lista 6
+#Exercicio 2
+
+
+
+str(print('----------------------'))
+str(print('  Repetição de frase  '))
+str(print('----------------------'))
+
+fat = 1
+cont = 1
+
+while (cont ):
+    num = float(input('Digite um numero => '))
+    fat * cont
