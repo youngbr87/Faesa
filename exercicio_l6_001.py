27cb94a590baa3ff6978ff534365b11f108a703a
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-10-19 19:32:17 -0300

    versao 1.0

---
+++ exercicio_l6_001.py
@@ -0,0 +1,10 @@
+cont = 0
+soma = 0
+
+while (cont < 850):
+    numero = float(input('Digite um numero => '))
+    soma = soma + numero
+    cont = cont + 1
+media = soma / cont
+
+str(print('A média é ', media))        
