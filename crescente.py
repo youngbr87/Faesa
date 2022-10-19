099ca9da69cd2ab96b60eeaa22427605b368ecef
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-10-19 12:18:03 -0300

    versao 1.0

---
+++ crescente.py
@@ -0,0 +1,21 @@
+#Autor: Vinicius Salles
+#Exercicio do curso de logica UDEMY
+#Foi feito primeiro no visualg
+
+
+
+num_1 = int(input('Digite um número => '))
+num_2 = int(input('Digite outro número => '))
+
+while num_1 != num_2:
+    if num_1 < num_2:
+        str(print('Crescente'))
+        break
+    else:
+        str(print('Decrescente'))
+        break
+            
+        
+
+    
+    
