ccbcdb9e7c1f8a822f4a6a4e73760c59f70077d5
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-10-19 12:25:57 -0300

    versao 1.0.1

--- crescente.py
+++ crescente.py
@@ -2,18 +2,31 @@
 #Exercicio do curso de logica UDEMY
 #Foi feito primeiro no visualg
 
+str(print('--------------------------------'))
+str(print(' Crescente decrescente          '))
+str(print('--------------------------------'))
 
-
-num_1 = int(input('Digite um número => '))
-num_2 = int(input('Digite outro número => '))
-
-while num_1 != num_2:
-    if num_1 < num_2:
-        str(print('Crescente'))
+print()
+while True:
+    num_1 = int(input('Digite um número => '))
+    print()
+    num_2 = int(input('Digite outro número => '))
+    print() 
+    while num_1 != num_2:
+        if num_1 < num_2:
+            str(print('Crescente'))
+            break
+        else:
+            str(print('Decrescente'))
+            break
+    print()
+    resp = str(input('Deseja continuar ? [S]/[N] => '))
+    if resp in 'N,n':
         break
-    else:
-        str(print('Decrescente'))
-        break
+ 
+
+print()
+str(print('Obrigado'))
             
         
 
