850d6cae69ddef8ecc91da004831dacfa2d7f499
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-10-26 19:20:19 -0300

    versao 1.0

---
+++ exercicio_l6_7.py
@@ -0,0 +1,16 @@
+#exericio unidade 3
+#Professora Renata Laranja
+#Desvio de condicional composto e aninhado
+#while for
+#Lista 6
+#Exercicio 7
+
+str(print('---------------------------------'))
+str(print('    Dois algoritmos A e B        '))
+str(print('---------------------------------'))
+
+
+
+num_a = float(input('Digite um número => '))
+num_b = float(input('Digite outro número => '))
+
