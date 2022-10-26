a9f209584a91a10b83c0603a0ba37209eab8827b
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-10-26 19:50:32 -0300

    versao 1.0

--- exercicio_l6_7.py
+++ exercicio_l6_7.py
@@ -5,14 +5,30 @@
 #Lista 6
 #Exercicio 7
 
+from re import A
+
+
 str(print('---------------------------------'))
 str(print('    Dois algoritmos A e B        '))
 str(print('---------------------------------'))
 
 
+soma = 0
 
-num_a = float(input('Digite um número => '))
-num_b = float(input('Digite outro número => '))
+a = int(input('Digite o valor de A => '))
+b = int(input('Digite o valor de B => '))
 
+if b > a:
+    for n in range(a + 1, b):
+       soma += n
 
+    str(print('A soma entre os numeros A e B é => ', soma))
 
+elif b < a:
+    for n in range(b + 1, a):
+       soma += n
+
+    str(print('A soma entre os numeros A e B é => ', soma))
+    
+else:
+    str(print('A e B são iguais'))        
