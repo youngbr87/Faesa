5d712394a8cb5635fea735cebe1f590e96c664d7
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-11-03 14:44:36 -0300

    versao 1.0

---
+++ exercicio_l6_11.py
@@ -0,0 +1,10 @@
+#exericio unidade 3
+#Professora Renata Laranja
+#Desvio de condicional composto e aninhado
+#while for
+#Lista 6
+#Exercicio 11
+
+str(print('---------------------------------'))
+str(print('                  '))
+str(print('---------------------------------'))
