1412226eeefd419deeaed5b686d2763eaa6a615b
Vinicius Salles - YoungBR <106876892+youngbr87@users.noreply.github.com>
2022-11-09 18:23:43 -0300

    1.0.1

--- exercicio_l6_5_f.py
+++ exercicio_l6_5_f.py
@@ -10,7 +10,6 @@ str(print('-----------------------------'))
 str(print('         Numero impar        '))
 str(print('-----------------------------'))
 
-
 for n in range(0, 21):
     if n % 2 == 1:
         print(n, 'é um número impar')
